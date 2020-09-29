005d0317b0b5f5614e8789f445d5f0082fd32a26
joedoyler <joedoyle678@gmail.com>
2020-09-29 20:23:36 +0000

    Stripe Feature Added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HWp9NDXYFLsbCR6d4fMSHWJp7lYGsjmd241zMcXPbxsr3T5lra2J8iL1WarnA2MSBJOyHf2P5aPgcWIBe8UsQfS00QogtdbFE',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
