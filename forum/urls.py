e4331d411801767ed0a7ffe276bcb5b943ad6100
joedoyler <joedoyle678@gmail.com>
2020-11-18 23:23:17 +0000

    Troubleshooting Heroku Internal Server Issue

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogs/', BlogListView, name='blogs'),
+    path('forum/', BlogListView, name='blogs'),
     path('blog/<int:_id>', BlogDetailView, name='blog'),
 ]
